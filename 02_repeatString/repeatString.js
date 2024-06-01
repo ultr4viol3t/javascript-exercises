bb0884f27e61d9a529121e34562f6a8e1fa6c22f
Ultr4viol3t <106609617+ultr4viol3t@users.noreply.github.com>
2024-06-01 11:20:45 +0200

    Finished the first 3 exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatNum) {
+    let finalString = ""
+    if (repeatNum < 0) {
+        finalString = "ERROR";
+    } else {
+        for (let i = 0; i < repeatNum; i++) {
+            finalString += text;
+          }
+    }
+    return finalString;
 };
+
+repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(text) {
+    return text.split("").reverse().join("");
+}
 // Do not edit below this line
 module.exports = reverseString;
